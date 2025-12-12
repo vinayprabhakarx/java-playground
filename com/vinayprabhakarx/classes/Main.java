2cced358b15efc197f1a679e2e8fe467b725f5c9
vianyprabhakarx <work.vinayprabhakar@gmail.com>
2025-12-12 14:55:11 +0530

    feat: Add Student and Main classes to demonstrate student properties and methods

---
+++ com/vinayprabhakarx/classes/Main.java
@@ -0,0 +1,43 @@
+package com.vinayprabhakarx.classes;
+
+/**
+ * Main class demonstrating the usage of Student class
+ * This program creates Student objects and demonstrates accessing their
+ * properties and methods
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        // Creating the first Student object
+        Student s1 = new Student();
+
+        // Setting properties for student 1
+        s1.id = 91;
+        s1.name = "Vinay Prabhakar";
+        s1.age = 22;
+
+        // Displaying student 1 details
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+
+        // Calling methods for student 1
+        s1.study();
+        s1.interviewPre();
+
+        // Creating the second Student object
+        Student s2 = new Student();
+
+        // Setting and displaying properties for student 2 (combined assignment and
+        // print)
+        System.out.println(s2.id = 96);
+        System.out.println(s2.name = "Santosh Kumar");
+        System.out.println(s2.age = 21);
+
+        // Calling methods for student 2
+        s2.study();
+        s2.interviewPre();
+
+    }
+
+}
---
+++ com/vinayprabhakarx/classes/Student.java
@@ -0,0 +1,30 @@
+package com.vinayprabhakarx.classes;
+
+/**
+ * Student class represents a student entity with basic properties and behaviors
+ */
+public class Student {
+    // Student unique identifier
+    int id;
+
+    // Student's full name
+    String name;
+
+    // Student's age
+    int age;
+
+    /**
+     * Method to simulate student studying behavior
+     */
+    void study() {
+        System.out.println("Student is studying");
+    }
+
+    /**
+     * Method to simulate student interview preparation
+     */
+    void interviewPre() {
+        System.out.println("Students are giving the interview");
+    }
+
+}
