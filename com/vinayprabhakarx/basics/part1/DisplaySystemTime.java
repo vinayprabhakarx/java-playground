cc8529fc07e17e06540a94f10ceb41a8283982e0
vianyprabhakarx <work.vinayprabhakar@gmail.com>
2025-12-21 21:53:17 +0530

    feat: Add DisplaySystemTime class to display current date and time

---
+++ com/vinayprabhakarx/basics/part1/DisplaySystemTime.java
@@ -0,0 +1,9 @@
+package com.vinayprabhakarx.basics.part1;
+
+public class DisplaySystemTime {
+
+    public static void main(String[] args) {
+        // Display the current date and time using System.currentTimeMillis()
+        System.out.format("\nCurrent Date time: %tc%n\n", System.currentTimeMillis());
+    }
+}
