efe87cebb3333a85f412212fe357c8a588e2672e
vinayprabhakarx <work.vinayprabhakar@gmail.com>
2026-03-26 14:43:41 +0530

    feat: add division exception handling

---
+++ src/com/vinayprabhakarx/exceptions/DivisionExceptionHandle.java
@@ -0,0 +1,29 @@
+package com.vinayprabhakarx.exceptions;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class DivisionExceptionHandle {
+    static void main(String[] args) {
+        System.out.println("Program Started!");
+        Scanner input = new Scanner(System.in);
+        try {
+            System.out.print("Enter the Numerator:");
+            double numerator = input.nextDouble();
+            System.out.print("Enter Denominator:");
+            double denominator = input.nextDouble();
+
+            double result = numerator / denominator;
+            System.out.println("The result of the Division is: " + result);
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter an integer");
+        } catch (ArithmeticException | NullPointerException e) {
+            System.out.println("Division by zero is not possible");
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        } finally {
+            input.close();
+            System.out.println("Program terminated!");
+        }
+    }
+}
